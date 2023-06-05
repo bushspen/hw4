11f35f7c24e2200edfe4468d86d422dd1ed6a43a
Spencer Bush <spencer.bush@kellogg.northwestern.edu>
2023-06-05 05:35:52 +0000

    adding changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,4 +15,13 @@ class PostsController < ApplicationController
     redirect_to "/places/#{@post["place_id"]}"
   end
 
+  # removes security restrictions for API calls
+  before_action :allow_cors
+  def allow_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Max-Age'] = '1728000'
+  end
+
 end
